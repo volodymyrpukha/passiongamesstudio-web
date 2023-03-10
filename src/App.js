db343924a8ec1bdd0727312ea868bd59a7eb7b26
Volodymyr <68910170+Voviksb@users.noreply.github.com>
2023-03-10 19:10:38 +0100

    done

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import React from 'react';
 function App() {
   const name = 'Volodymyr Pukha';
   const year = new Date().getFullYear();
+  const address = '40 , New Bond Street, W1S 2RX, London, United Kingdom';
+  const phone = '+44 7700150703';
   return (
-    <div style={{textAlign: 'center'}}>
+    <div style={{textAlign: 'center'}} class="main">
       <h1>MYPULLUP LIMITED</h1>
       <p>{name}</p>
+      <p>Game Developer Studio</p>
+      <p>{address}</p>
+      <p>{phone}</p>
       <p>{year} Copyright. All Rights Reserved.</p>
     </div>
   );
